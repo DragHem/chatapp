e85981fce430fde336a6f18797fc79a0d54d5428
Damian Puś <pusiek99@interia.pl>
2023-05-10 12:34:26 +0200

    Setup index page, installed react-icon

---
+++ app/(site)/page.tsx
@@ -0,0 +1,33 @@
+import { ImBubbles } from 'react-icons/im';
+
+export default function Home() {
+  return (
+    <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+    "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-x-md">
+        <ImBubbles className="mx-auto w-auto h-24 text-sky-400" />
+        <h2
+          className="
+        mt-6
+        text-center
+        text-3xl
+        font-bold
+        tracking-tight
+        text-gray-900"
+        >
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
+}
