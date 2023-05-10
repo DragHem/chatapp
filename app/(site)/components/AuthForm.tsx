934f0f0f907eebcd2773ccaf037ba199ba349cb0
Damian Puś <pusiek99@interia.pl>
2023-05-10 13:29:23 +0200

    Created button component

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,6 +3,7 @@
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Input from '@/app/components/Input';
+import Button from '@/app/components/Button';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -47,7 +48,32 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmitHandler)}>
-          <Input id="email" label="Email" register={register} errors={errors} />
+          {variant === 'REGISTER' && (
+            <Input
+              id="name"
+              label="Name"
+              type="text"
+              register={register}
+              errors={errors}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button>Test</Button>
+          </div>
         </form>
       </div>
     </div>
