e7904607c371143d32ef54bccbaf0ed64b61b8a9
Damian Puś <pusiek99@interia.pl>
2023-05-10 12:45:04 +0200

    Added empty handlers in AuthForm

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,48 @@
 'use client';
 
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
 const AuthForm = () => {
-  return <div>Auth Form</div>;
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariantHandler = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    }
+
+    setVariant('LOGIN');
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmitHandler: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+    }
+    if (variant === 'LOGIN') {
+    }
+  };
+
+  const socialActions = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return <div>Auth Form!</div>;
 };
 
 export default AuthForm;
