59ca52edb9093a8d880e418310eec2327fa5066e
Damian Puś <pusiek99@interia.pl>
2023-05-10 13:13:27 +0200

    Created input component

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Input from '@/app/components/Input';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -42,7 +43,15 @@ const AuthForm = () => {
     setIsLoading(true);
   };
 
-  return <div>Auth Form!</div>;
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmitHandler)}>
+          <Input id="email" label="Email" register={register} errors={errors} />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
